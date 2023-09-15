8c05114249e015a5531bb449ac8fdbb939cda0bf
dan38 <danilo3840@gmail.com>
2023-09-15 20:11:31 -0300

    Adapt to LMODroid

--- lmodroid_xpeng.mk
+++ lmodroid_xpeng.mk
@@ -11,10 +11,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from xpeng device
 $(call inherit-product, device/motorola/xpeng/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common lmodroid stuff.
+$(call inherit-product, vendor/lmodroid/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_xpeng
+PRODUCT_NAME := lmodroid_xpeng
 PRODUCT_DEVICE := xpeng
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_BRAND := motorola
